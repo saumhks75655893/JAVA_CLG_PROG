43259ac6581f01e560192a6636cfff9eeaf1df6c
himanshukumar76 <himanshukumar75655893@gmail.com>
2024-09-05 12:35:12 +0530

    new files added

--- basic.java
+++ basic.java
@@ -1,5 +1,7 @@
 public class basic {
     public static void main(String[] args) {
-        System.out.println(5 + 10);
+        System.out.println("Hello World !!!");
+        System.out.println("This is me Himanshu kumar"); 
+        System.out.print("Hi, This is me."); 
     }
 }
